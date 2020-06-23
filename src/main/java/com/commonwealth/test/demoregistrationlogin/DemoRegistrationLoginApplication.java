29f9359b2571a9e59e501e17261615bc26f0a3a2
senaersania <senaersania@gmail.com>
2020-06-23 11:18:45 +0700

    first commit

---
+++ src/main/java/com/commonwealth/test/demoregistrationlogin/DemoRegistrationLoginApplication.java
@@ -0,0 +1,13 @@
+package com.commonwealth.test.demoregistrationlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRegistrationLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRegistrationLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commonwealth/test/demoregistrationlogin/DemoRegistrationLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commonwealth.test.demoregistrationlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRegistrationLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
